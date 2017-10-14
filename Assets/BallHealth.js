42613f37b3c0bd8bbe73f79ddf508ec0c7a31abc
Mohit Ahuja <ahujamoh@gmail.com>
2017-10-14 23:46:54 +0200

    adding audio file

--- Assets/BallHealth.js
+++ Assets/BallHealth.js
@@ -5,10 +5,24 @@
 //}
 
 var maxDepthAllowed = -10;
+var isRestarting = false;
 function Update () {
 	if(transform.position.y <= maxDepthAllowed)
 	{
-		Debug.Log("Test");
-		Application.LoadLevel("Level-01");
+		if (isRestarting == false){
+			Debug.Log("Test");
+			RestartLevel();
+		}
 	}
+}
+
+function RestartLevel(){
+	Debug.Log("Test2");
+	isRestarting = true;
+	Debug.Log(GetComponent.<AudioSource>().clip.length);
+	GetComponent.<AudioSource>().Play();
+	yield WaitForSeconds(2 * GetComponent.<AudioSource>().clip.length);
+	Debug.Log(GetComponent.<AudioSource>().clip.length);
+	Application.LoadLevel("Level-01");
+	
 }
