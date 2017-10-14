33f8422533409189e7f454f618b1869b324168f8
Mohit Ahuja <ahujamoh@gmail.com>
2017-10-14 21:44:55 +0200

    setting time for the coin effect to 3.5 s without looping

--- Assets/CoinPickup.js
+++ Assets/CoinPickup.js
@@ -7,9 +7,12 @@
 //function Update () {
 //	
 //}
+var coinEffect : Transform; //use Transform to a
+
 function OnTriggerEnter(info: Collider){
 	if(info.tag == "Player"){
 		Debug.Log("Coin Picked Up");
+		Instantiate(coinEffect, transform.position, transform.rotation);
 		Destroy(gameObject);
 	}
 }
