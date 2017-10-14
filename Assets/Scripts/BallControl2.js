aa4bc51de866f3e4e61e5dd60c1389273299f79f
Mohit Ahuja <ahujamoh@gmail.com>
2017-10-14 18:43:13 +0200

    till 5th video

---
+++ Assets/Scripts/BallControl2.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+//function Start () {
+//	
+//}
+private var isFalling = false;
+var rotationSpeed = 100;
+var jumpHeight = 8;
+function Update () {
+	var rotation : float = Input.GetAxis("Horizontal")*rotationSpeed;
+	rotation *= Time.deltaTime;
+	GetComponent.<Rigidbody>().AddRelativeTorque(Vector3.back * rotation);
+
+	if(Input.GetKeyDown(KeyCode.W) && isFalling == false){
+		GetComponent.<Rigidbody>().velocity.y = jumpHeight;
+	}
+	isFalling = true;
+}
+
+function OnCollisionStay(){
+	isFalling = false;
+}
