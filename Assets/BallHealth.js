44ee6edb7390f985d34d8ee7d373192884cbb94b
Mohit Ahuja <ahujamoh@gmail.com>
2017-10-14 20:11:16 +0200

    till video 8

---
+++ Assets/BallHealth.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+//function Start () {
+//	
+//}
+
+var maxDepthAllowed = -10;
+function Update () {
+	if(transform.position.y <= maxDepthAllowed)
+	{
+		Debug.Log("Test");
+		Application.LoadLevel("Level-01");
+	}
+}
---
+++ Assets/CoinPickup.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+//function Start () {
+//	
+//}
+//
+//function Update () {
+//	
+//}
+function OnTriggerEnter(info: Collider){
+	if(info.tag == "Player"){
+		Debug.Log("Coin Picked Up");
+		Destroy(gameObject);
+	}
+}
