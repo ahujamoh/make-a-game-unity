bc13fc563644a80a9fc50a7165fcebc61cb24361
Mohit Ahuja <ahujamoh@gmail.com>
2017-10-14 23:18:05 +0200

    adding audio file

--- Assets/CoinPickup.js
+++ Assets/CoinPickup.js
@@ -1,18 +1,13 @@
 ﻿#pragma strict
 
-//function Start () {
-//	
-//}
-//
-//function Update () {
-//	
-//}
 var coinEffect : Transform; //use Transform to a
 
 function OnTriggerEnter(info: Collider){
 	if(info.tag == "Player"){
 		Debug.Log("Coin Picked Up");
+		GameMaster.currentScore += 1;
 		Instantiate(coinEffect, transform.position, transform.rotation);
+//		Destroy(effect.gameObject, 3); //stop particle here after 3 second
 		Destroy(gameObject);
 	}
 }
---
+++ Assets/GameMaster.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+static var currentScore : int = 0;
+//var test = 0;
+
+//function Update(){
+//	test = currentScore;
+//}
+
+var offSetY : float = 40;
+var sizeX : float = 100;
+var sizeY : float = 40;
+
+function Start(){
+	currentScore = 0;
+}
+
+function OnGUI(){
+	GUI.Label(new Rect(Screen.height * 0.5 + sizeX/2, offSetY, sizeX, sizeY), "Score : "+currentScore);
+}
